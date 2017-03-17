2ba36b5a2a8c75646e2d623b5de884a2268a9855
Dean <zhang.fuwen@qq.com>
2017-03-17 20:31:35 +0800

    restful service is working

--- src/main/java/gateway/Application.java
+++ src/main/java/gateway/Application.java
@@ -3,20 +3,17 @@ package gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-import java.io.IOException;
-
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            Gateway gateway = new Gateway("./config.xml");
-            gateway.run();
-        }catch (IOException e) {
-            System.out.println("gateway error:"+e.getMessage());
-            e.printStackTrace();
-        }
+//        try {
+//            Gateway gateway = new Gateway("./config.xml");
+//            gateway.run();
+//        }catch (IOException e) {
+//            System.out.println("gateway error:"+e.getMessage());
+//            e.printStackTrace();
+//        }
         SpringApplication.run(Application.class, args);
     }
 }
