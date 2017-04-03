f899deddae1c8678d481cc1c762d0b7ad27e47be
Dean <zhang.fuwen@qq.com>
2017-04-03 23:27:13 +0800

    启动虚拟读取

--- src/main/java/com/zhangfuwen/Application.java
+++ src/main/java/com/zhangfuwen/Application.java
@@ -1,11 +1,19 @@
 package com.zhangfuwen;
 
+import com.zhangfuwen.collector.Config;
+import com.zhangfuwen.collector.Gateway;
+import com.zhangfuwen.collector.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 @RestController
@@ -24,27 +32,27 @@ public class Application extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
-//        Config config = Config.getInstance();
-//        Gateway com.zhangfuwen.collector = config.getGateway();
-//        if(!config.isDevMode()) {
-//            try {
-//                com.zhangfuwen.collector.init();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                return;
-//            }
-//        }
-//        new Timer().schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//                System.out.println("scheduled to collect at " + new Date());
-//                try {
-//                    com.zhangfuwen.collector.collectAndPersist(config.isDevMode(), Persistence.getEntityManager());
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        },0, config.getInterval()*1000);
+        Config config = Config.getInstance();
+        Gateway gateway = config.getGateway();
+        if(!config.isDevMode()) {
+            try {
+                gateway.init();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("scheduled to collect at " + new Date());
+                try {
+                    gateway.collectAndPersist(config.isDevMode(), Persistence.getEntityManager());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        },0, config.getInterval()*1000);
 
         SpringApplication.run(Application.class, args);
     }
